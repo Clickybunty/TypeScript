7e01bc4811cc8edd53e99f8bd94115342ad18ea7
ClickyBunty <stevan.menicnain@yahoo.de>
2022-04-05 22:48:16 +0200

    added new image

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
-doSomething();
+var message;
+message = 'abc';
+var endsWithC = message.endsWith('c');
+var alternativeWay = message.endsWith('c');
