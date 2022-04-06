3eb95c132df833ca1354129ed2426c2bfed02189
ClickyBunty <stevan.menicnain@yahoo.de>
2022-04-06 10:57:10 +0200

    lambda funktion

--- Main.ts
+++ Main.ts
@@ -1,5 +1,4 @@
-
-let message;
-message = 'abc';
-let endsWithC = (<string>message).endsWith('c');
-let alternativeWay = (message as string).endsWith('c');
+let log = function(message) {
+    console.log(message);
+}
+let doLog = (message) => console.log(message);
